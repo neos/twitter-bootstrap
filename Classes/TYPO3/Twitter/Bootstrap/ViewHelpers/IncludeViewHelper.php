1c293d6cc689da5e9ff5d93e2e4d91970d3139ac
Robert Lemke <robert@typo3.org>
2013-08-28 18:05:32 +0200

    [TASK] Adjust to new Flow Resource Management
    Change-Id: I10f70284f1a3e162b23f9f1e5e19e8b6dca155ca
    Releases: master
    Related: FLOW-108
    Reviewed-on: http://review.typo3.org/24499
    Reviewed-by: Karsten Dambekalns <karsten@typo3.org>
    Tested-by: Karsten Dambekalns <karsten@typo3.org>
    Reviewed-by: Robert Lemke <robert@typo3.org>
    Tested-by: Robert Lemke <robert@typo3.org>
    Reviewed-by: Christian Müller <christian@kitsunet.de>
    Tested-by: Christian Müller <christian@kitsunet.de>

--- Classes/TYPO3/Twitter/Bootstrap/ViewHelpers/IncludeViewHelper.php
+++ Classes/TYPO3/Twitter/Bootstrap/ViewHelpers/IncludeViewHelper.php
@@ -15,10 +15,10 @@ use TYPO3\Flow\Annotations as Flow;
 class IncludeViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
 
 	/**
-	 * @var \TYPO3\Flow\Resource\Publishing\ResourcePublisher
+	 * @var \TYPO3\Flow\Resource\ResourceManager
 	 * @Flow\Inject
 	 */
-	protected $resourcePublisher;
+	protected $resourceManager;
 
 	/**
 	 * Get the header include code for including Twitter Bootstrap on a page. If needed
@@ -37,19 +37,19 @@ class IncludeViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper 
 	public function render($version, $minified = TRUE, $includeJQuery = FALSE, $jQueryVersion = '1.10.1') {
 		$content = sprintf(
 			'<link rel="stylesheet" href="%s" />' . PHP_EOL,
-			$this->resourcePublisher->getStaticResourcesWebBaseUri() . 'Packages/TYPO3.Twitter.Bootstrap/' . $version . '/css/bootstrap' . ($minified === TRUE ? '.min' : '') . '.css'
+			$this->resourceManager->getPublicPackageResourceUri('TYPO3.Twitter.Bootstrap', $version . '/css/bootstrap' . ($minified === TRUE ? '.min' : '') . '.css')
 		);
 
 		if ($includeJQuery === TRUE) {
 			$content .= sprintf(
 				'<script src="%s"></script>' . PHP_EOL,
-				$this->resourcePublisher->getStaticResourcesWebBaseUri() . 'Packages/TYPO3.Twitter.Bootstrap/Libraries/jQuery/jquery-' . $jQueryVersion . ($minified === TRUE ? '.min' : '') . '.js'
+				$this->resourceManager->getPublicPackageResourceUri('TYPO3.Twitter.Bootstrap', 'Libraries/jQuery/jquery-' . $jQueryVersion . ($minified === TRUE ? '.min' : '') . '.js')
 			);
 		}
 
 		$content .= sprintf(
 			'<script src="%s"></script>' . PHP_EOL,
-			$this->resourcePublisher->getStaticResourcesWebBaseUri() . 'Packages/TYPO3.Twitter.Bootstrap/' . $version . '/js/bootstrap' . ($minified === TRUE ? '.min' : '') . '.js'
+				$this->resourceManager->getPublicPackageResourceUri('TYPO3.Twitter.Bootstrap', $version . '/js/bootstrap' . ($minified === TRUE ? '.min' : '') . '.js')
 		);
 
 		return $content;
